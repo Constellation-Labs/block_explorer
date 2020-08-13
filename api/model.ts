972f1f0cff6a1cdbc59b53f6a0bc177e6218a4c3
Mateusz Wadoń <mwadon@gmail.com>
2020-08-14 00:43:22 +0200

    ES connection and snapshot endpoints

---
+++ api/model.ts
@@ -0,0 +1,5 @@
+export type Snapshot = {
+    checkpointBlocks: string[]
+    hash: string
+    height: number
+}
---
+++ api/validation.ts
@@ -0,0 +1,61 @@
+import {APIGatewayEvent} from 'aws-lambda'
+import {ApplicationError, StatusCodes} from './http'
+import {chain, left, of, right, TaskEither} from 'fp-ts/lib/TaskEither'
+import {pipe} from 'fp-ts/lib/pipeable'
+
+const bodyNotNull = (event: APIGatewayEvent) => {
+    if (event.body === null) {
+        return left<ApplicationError, APIGatewayEvent>(
+            new ApplicationError(
+                'Error parsing request body',
+                ['Body cannot be empty'],
+                StatusCodes.BAD_REQUEST
+            )
+        )
+    }
+    return right<ApplicationError, APIGatewayEvent>(event)
+}
+
+const queryParamsIsNull = (event: APIGatewayEvent) => {
+    if (event.queryStringParameters !== null) {
+        return left<ApplicationError, APIGatewayEvent>(
+            new ApplicationError(
+                'Error parsing request query params',
+                ['Query params should be empty'],
+                StatusCodes.BAD_REQUEST
+            )
+        )
+    }
+    return right<ApplicationError, APIGatewayEvent>(event)
+}
+
+const pathParamsIsNotNull = (event: APIGatewayEvent) => {
+    if (event.pathParameters === null) {
+        return left<ApplicationError, APIGatewayEvent>(
+            new ApplicationError(
+                'Error parsing request path params',
+                ['Path params should not be empty'],
+                StatusCodes.BAD_REQUEST
+            )
+        )
+    }
+    return right<ApplicationError, APIGatewayEvent>(event)
+}
+
+const termIsNotNull = (event: APIGatewayEvent) => pipe(
+    of<ApplicationError, APIGatewayEvent>(event),
+    chain(pathParamsIsNotNull),
+    chain(() => event.pathParameters!.hash === null
+        ? left<ApplicationError, APIGatewayEvent>(
+            new ApplicationError(
+                'Error parsing request path params',
+                ['Term param should not be empty'],
+                StatusCodes.BAD_REQUEST))
+        : right<ApplicationError, APIGatewayEvent>(event))
+)
+
+export const validateListSnapshotsEvent = (event: APIGatewayEvent) =>
+    pipe(
+        of<ApplicationError, APIGatewayEvent>(event),
+        chain(termIsNotNull)
+    )
