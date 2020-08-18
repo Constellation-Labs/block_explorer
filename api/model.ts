3dd0a3fbdcadbf05d478d8cd314fee276bd7b9c5
Mateusz Wadoń <mwadon@gmail.com>
2020-08-18 09:59:21 +0200

    Implement checkpoint-blocks endpoint

--- api/model.ts
+++ api/model.ts
@@ -2,4 +2,23 @@ export type Snapshot = {
     checkpointBlocks: string[]
     hash: string
     height: number
+    timestamp: string
+}
+
+export type CheckpointBlock = {
+    hash: string
+    height: Height
+    transactions: string[]
+    notifications: string[]
+    observations: string[]
+    children: number
+    snapshotHash: string
+    soeHash: string
+    parentSOEHashes: string[]
+    timestamp: string
+}
+
+type Height = {
+    min: number,
+    max: number
 }
--- api/service.ts
+++ api/service.ts
@@ -3,8 +3,8 @@ import {ApplicationError, errorResponse, StatusCodes, successResponse} from './h
 import {APIGatewayEvent} from 'aws-lambda'
 import {chain, fold, taskEither, map} from 'fp-ts/lib/TaskEither'
 import {pipe} from 'fp-ts/lib/pipeable'
-import {getSnapshot} from './elastic'
-import {validateListSnapshotsEvent} from './validation'
+import {getSnapshot, getCheckpointBlock} from './elastic'
+import {validateListCheckpointBlocksEvent, validateListSnapshotsEvent} from './validation'
 
 export const getSnapshotHandler = (event: APIGatewayEvent, es: Client) =>
     pipe(
@@ -12,6 +12,18 @@ export const getSnapshotHandler = (event: APIGatewayEvent, es: Client) =>
         chain(validateListSnapshotsEvent),
         map(event => event.pathParameters!.term),
         chain(getSnapshot(es)),
+        fold(
+            reason => taskEither.of(errorResponse(reason)),
+            value => taskEither.of(successResponse(StatusCodes.OK)(value))
+        )
+    )
+
+export const getCheckpointBlocksHandler = (event: APIGatewayEvent, es: Client) =>
+    pipe(
+        taskEither.of<ApplicationError, APIGatewayEvent>(event),
+        chain(validateListCheckpointBlocksEvent),
+        map(event => event.pathParameters!.term),
+        chain(getCheckpointBlock(es)),
         fold(
             reason => taskEither.of(errorResponse(reason)),
             value => taskEither.of(successResponse(StatusCodes.OK)(value))
