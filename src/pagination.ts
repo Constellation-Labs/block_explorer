bc0d620d31db7a7917d5a95dcff8c0d23506af0f
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2025-03-12 19:03:29 -0300

    use postgres and prisma for dag, metagraph, and actions endpoints

---
+++ src/pagination.ts
@@ -0,0 +1,106 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { handleError, respond } from "./response";
+import { Pagination } from './request-params';
+import { toNumber, isFinite } from "lodash";
+
+export const maxSizeLimit = 100;
+
+export enum SortOrder {
+  Desc = 'desc',
+  Asc = 'asc'
+}
+
+export enum SearchDirection {
+  After = 'search_after',
+  Before = 'search_before'
+}
+
+
+const safeNumber = (value, defaultValue) => {
+  const num = toNumber(value);
+  return (isFinite(num) && num > 0) ? num : defaultValue;
+};
+
+export const toCreatedAtCursor = (row) => ({ created_at: new Date(row.created_at) });
+const fromCreatedAtCursor = (row) => ({
+  created_at: row.created_at.toISOString()
+});
+
+export const toOrdinalCursor = (row) => ({ ordinal: BigInt('0x' + row.ordinal) });
+export const fromOrdinalCursor = (row) => ({ ordinal: row.ordinal.toString(16) });
+
+export const toCreatedAtOrdinalCursor = (row) => ({
+  ...toCreatedAtCursor(row),
+  ...toOrdinalCursor(row)
+});
+export const fromCreatedAtOrdinalCursor = (row) => ({
+  ...fromCreatedAtCursor(row),
+  ...fromOrdinalCursor(row)
+});
+
+
+const buildPageQuery = <T>(pagination: Pagination, nextToCursor) => {
+  const pageSize = safeNumber(pagination.size, maxSizeLimit);
+  const incrementedSize = pageSize + 1;
+
+  if (
+    pagination &&
+    'searchSince' in pagination && pagination.searchSince &&
+    'searchDirection' in pagination && pagination.searchDirection
+  ) {
+    const page =
+      pagination.searchDirection === SearchDirection.Before
+        ? -incrementedSize
+        : incrementedSize;
+    return {
+      take: page,
+      cursor: pagination.searchSince
+    };
+  } 
+
+  if (pagination && 'next' in pagination) {
+    return { take: incrementedSize, cursor: nextToCursor(pagination.next) };
+  }
+
+  if (pagination && pagination.size) {
+    return { take: incrementedSize };
+  } 
+
+  return { take: incrementedSize };
+};
+
+export const paginatedQuery = async (
+  pagination,
+  nextToCursor,
+  cursorToNext,
+  baseQuery,
+  findMany,
+  transformResponse
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const pageQueryParams = buildPageQuery(pagination, nextToCursor);
+
+    const pagedQuery = {
+      ...baseQuery,
+      ...(pagination ? pageQueryParams : {})
+    };
+
+    const rawResults = await findMany(pagedQuery);
+
+    const pageSize = pageQueryParams.take
+      ? pageQueryParams.take - 1
+      : maxSizeLimit;
+    const hasNextPage = rawResults.length > pageSize;
+
+    let result = rawResults;
+    let next;
+    if (hasNextPage) {
+      result = rawResults.slice(0, -1);
+      const last = rawResults.at(-1);
+      next = cursorToNext(last);
+    }
+    return respond(result, transformResponse, next);
+  } catch (error) {
+    return handleError(error);
+  }
+};
