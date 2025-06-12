b1dc9f02cdf30a34e6616ba81e47560ec20c0dc6
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2025-06-11 21:50:13 -0300

    fix snapshot count typo

--- tests/handlers/dagHandler.test.ts
+++ tests/handlers/dagHandler.test.ts
@@ -192,7 +192,7 @@ describe("DAG Handler Integration Tests", () => {
       expect(snapshot.subHeight).toBe(Number(testSnapshot.subheight));
       expect(Array.isArray(snapshot.blocks)).toBe(true);
       expect(snapshot.timestamp).toBeDefined();
-      expect(snapshot.metagraphSnashotCount).toBe(
+      expect(snapshot.metagraphSnapshotCount).toBe(
         Number(testSnapshot.metagraph_snapshot_count)
       );
     });
